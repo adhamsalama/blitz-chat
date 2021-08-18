bcab1f0be79c6a07363f783e4b854dbf6f99c8be
Adham Salama <adhamsalama@zohomail.com>
2021-08-18 19:30:10 +0200

    Use authenticateToken as middleware for router and add authentication for entering rooms

--- frontend/views.js
+++ frontend/views.js
@@ -10,20 +10,24 @@ router.get('/accounts', (req, res) => {
     res.render('signing')
 })
 
+router.use(authenticateToken)
 
-router.get('/', authenticateToken, async (req, res) => {
+router.get('/', async (req, res) => {
     const rooms = await Room.find({type: 'public'})
     const userRooms = await Room.find({creator: req.user.username})
-    res.render('index', {rooms: rooms, userRooms: userRooms})
+    const joinedRooms = await Room.find({users: req.user.username})
+    res.render('index', {rooms: rooms, userRooms: userRooms, joinRooms: joinedRooms})
 
 })
 
-router.get('/rooms/:room', authenticateToken, async (req, res) => {
+router.get('/rooms/:room', async (req, res) => {
     const room = await Room.findOne({name: req.params.room})
-    res.render('room', {room: room})
+    if(room.type == 'public' || room.creator == req.user.username || room.users.indexOf(req.user.username) >= 0)
+        return res.render('room', {room: room})
+    res.sendStatus(403)
 })
 
-router.get('/users/:user', authenticateToken, async (req, res) => {
+router.get('/users/:user', async (req, res) => {
     res.render('user')
 })
 
